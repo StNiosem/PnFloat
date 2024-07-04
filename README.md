01d1842430ed0ae95707a44e1960b15c522dd01a
StNiosem <bauch.aristide@gmail.com>
2024-07-04 23:58:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# The PnFloat Window Manager
+It manages windows on Wayland. There you go.
+
+Part of the [PnOS](https://github.com/StNiosem/PnOS) ecosystem
---
+++ headers/pnFloat.h
@@ -0,0 +1,15 @@
+
+// Just the basics you know, 10 HEADERS FOR WHAT?
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <standards.h>
+
+
+#define MaxScreenHeightPixels 8640
